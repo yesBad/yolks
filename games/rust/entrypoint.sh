e3bcdb3b3a631d51e930d3c16e3f721b3bcaed15
Bad <87938689+yesBad@users.noreply.github.com>
2026-07-11 22:58:28 +0300

    Swap entrypoint.sh to use DepotDownloader

--- games/rust/entrypoint.sh
+++ games/rust/entrypoint.sh
@@ -4,40 +4,37 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
-
-## if auto_update is not set or to 1 update
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
-	./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update 258550 +quit
+    [[ -n "${SRCDS_BETAID:-}" ]] && args+=(-branch "${SRCDS_BETAID}")
+    [[ -n "${SRCDS_BETAPW:-}" ]] && args+=(-branchpassword "${SRCDS_BETAPW}")
+    ./DepotDownloader/DepotDownloader -app 258550 "${args[@]}" -dir /home/container -os linux -validate
 else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
-# Replace Startup Variables
+# Replace startup variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+# Install selected framework
 if [[ "${FRAMEWORK}" == "carbon" ]]; then
-    # Carbon: https://github.com/CarbonCommunity/Carbon.Core
     echo "Updating Carbon..."
-    curl -sSL "https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz" | tar zx
-    echo "Done updating Carbon!"
-
+    curl -fsSL "https://github.com/CarbonCommunity/Carbon.Core/releases/download/production_build/Carbon.Linux.Release.tar.gz" | tar -xz
     export DOORSTOP_ENABLED=1
     export DOORSTOP_TARGET_ASSEMBLY="$(pwd)/carbon/managed/Carbon.Preloader.dll"
     MODIFIED_STARTUP="LD_PRELOAD=$(pwd)/libdoorstop.so ${MODIFIED_STARTUP}"
-
-elif [[ "$OXIDE" == "1" ]] || [[ "${FRAMEWORK}" == "oxide" ]]; then
-    # Oxide: https://github.com/OxideMod/Oxide.Rust
+    echo "Done updating Carbon!"
+fi
+if [[ "${FRAMEWORK}" == "oxide" ]]; then
     echo "Updating uMod..."
-    curl -sSL "https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip" > umod.zip
-    unzip -o -q umod.zip
-    rm umod.zip
+    curl -fsSL "https://github.com/OxideMod/Oxide.Rust/releases/latest/download/Oxide.Rust-linux.zip" -o umod.zip
+    unzip -oq umod.zip
+    rm -f umod.zip
     echo "Done updating uMod!"
-# else Vanilla, do nothing
 fi
 
-# Fix for Rust not starting
-export LD_LIBRARY_PATH=$(pwd)/RustDedicated_Data/Plugins/x86_64:$(pwd)
+# Rust library paths
+export LD_LIBRARY_PATH="$(pwd)/RustDedicated_Data/Plugins/x86_64:$(pwd)"
 
 # Run the Server
 node /wrapper.js "${MODIFIED_STARTUP}"
